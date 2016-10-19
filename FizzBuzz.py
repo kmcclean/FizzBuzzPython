5532f505e4e91620e076a43972adfd7df7f3d98f
Kevin McClean <mcclean@gmail.com>
2016-10-18 19:30:46 -0500

    first commit

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+def main():
+    a = 0
+
+    while a < 100:
+        a += 1
+        if a % 15 == 0:
+            print("fizzbuzz")
+        elif a % 5 == 0:
+            print("buzz")
+        elif a % 3 == 0:
+            print("fizz")
+        else:
+            print(a)
+
+
+main()
